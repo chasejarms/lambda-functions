1974121bf03292cdaf089e8075d18e38ef597fa4
Chase <chasejarms@gmail.com>
2021-06-14 11:31:56 -0700

    Try this out

--- code/src/app.ts
+++ code/src/app.ts
@@ -1,5 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { HttpStatusCode } from "./models/httpStatusCode";
+import * as AWSCognitoIdentity from "amazon-cognito-identity-js";
+
+const poolData = {
+    UserPoolId: "us-east-1_rh9H8whcD", // Your user pool id here
+    ClientId: "6gc6tovbdh8k56o4svgec2kdih", // Your client id here
+};
+const userPool = new AWSCognitoIdentity.CognitoUserPool(poolData);
 
 /**
  * The purpose of this function is just to sign up new users (i.e. never have been added to the system). If
@@ -33,12 +40,12 @@ export const signUpNewUserHandler = async (
         };
     }
 
-    const { companyName, username, password, name } = JSON.parse(event.body);
+    const { companyName, email, password, name } = JSON.parse(event.body);
 
-    if (!companyName || !username || !password || !name) {
+    if (!companyName || !email || !password || !name) {
         const requiredFieldsNotProvidedResponse = JSON.stringify({
             message:
-                "companyName, username, name, and password are required fields",
+                "companyName, email, name, and password are required fields",
         });
 
         return {
@@ -47,7 +54,52 @@ export const signUpNewUserHandler = async (
         };
     }
 
-    // check to see if the username and password already exist
-    // if they don't exist, try creating them
+    const attributeList = [];
+    attributeList.push(
+        new AWSCognitoIdentity.CognitoUserAttribute({
+            Name: "name",
+            Value: name,
+        })
+    );
+
+    let userSignUpResponse: null | APIGatewayProxyResult = null;
+    let callbackComplete = false;
+
+    const callback: AWSCognitoIdentity.NodeCallback<
+        Error,
+        AWSCognitoIdentity.ISignUpResult
+    > = (error, signUpResult) => {
+        if (error) {
+            userSignUpResponse = {
+                statusCode: HttpStatusCode.BadRequest,
+                body: JSON.stringify({
+                    message: error.message,
+                }),
+            };
+        }
+
+        callbackComplete = true;
+    };
+
+    userPool.signUp(email, password, attributeList, [], callback);
+
+    while (!callbackComplete) {
+        await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 20);
+        });
+    }
+
+    if (userSignUpResponse !== null) {
+        return userSignUpResponse;
+    }
+
+    return {
+        statusCode: HttpStatusCode.Ok,
+        body: JSON.stringify({
+            message: "Got pretty far on this one right?",
+        }),
+    };
     // if they are successfully created, go ahead and create the company and the user in dynamo db
 };
