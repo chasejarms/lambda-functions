52838a4c5ca918ad095c0daa7de525254aa886b6
Chase <chasejarms@gmail.com>
2021-06-22 20:38:02 -0600

    Get started on get companies for user call

--- code/src/functions/getCompaniesForUser/index.ts
+++ code/src/functions/getCompaniesForUser/index.ts
@@ -1,13 +1,30 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { HttpStatusCode } from "../../models/httpStatusCode";
+import { userSubFromEvent } from "../../utils/userSubFromEvent";
+import * as AWS from "aws-sdk";
 
 export const getCompaniesForUser = async (
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+    const userSub = userSubFromEvent(event);
+    if (userSub === "") {
+        return {
+            statusCode: HttpStatusCode.BadRequest,
+            body: JSON.stringify({
+                message: "Issue getting the user sub from the event",
+            }),
+        };
+    }
+
+    const dynamoClient = new AWS.DynamoDB.DocumentClient();
+    dynamoClient.query({
+        TableName: "primaryTableOne",
+    });
+
     return {
         statusCode: HttpStatusCode.Ok,
         body: JSON.stringify({
-            message: "We got past the guards",
+            message,
         }),
     };
 };
---
+++ code/src/utils/userSubFromEvent.ts
@@ -0,0 +1,19 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import jwt_decode, { JwtPayload } from "jwt-decode";
+
+export function userSubFromEvent(event: APIGatewayProxyEvent) {
+    const authHeader = event.headers.AuthHeader;
+
+    if (!authHeader) {
+        console.log("userSubFromEvent: auth header is not present on request");
+        return "";
+    }
+
+    const decodedToken = jwt_decode(authHeader) as JwtPayload;
+    if (!decodedToken.sub) {
+        console.log("userSubFromEvent: subject is not present on token");
+        return "";
+    }
+
+    return decodedToken.sub;
+}
