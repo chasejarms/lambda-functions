092dd2d482ed253a46e84e1e03a9affa1db3aafb
Chase <chasejarms@gmail.com>
2021-06-24 11:33:04 -0600

    Adjust company user for new model

--- code/src/utils/isCompanyUserAdmin/index.ts
+++ code/src/utils/isCompanyUserAdmin/index.ts
@@ -3,6 +3,7 @@ import { userSubFromEvent } from "../userSubFromEvent";
 import * as AWS from "aws-sdk";
 import { primaryTableName } from "../../constants/primaryTableName";
 import { ICompanyUser } from "../../models/companyUser";
+import { IDefaultPrimaryTableModel } from "../../models/defaultPrimaryTableModel";
 
 export async function isCompanyUserAdmin(
     event: APIGatewayProxyEvent,
@@ -11,21 +12,24 @@ export async function isCompanyUserAdmin(
     const userSub = userSubFromEvent(event);
 
     const dynamoClient = new AWS.DynamoDB.DocumentClient();
+    const primaryTableKeySearch: IDefaultPrimaryTableModel = {
+        itemId: `COMPANYUSER.${userSub}`,
+        belongsTo: `COMPANY.${companyId}`,
+    };
     try {
         const getCompanyUserResult = await dynamoClient
             .get({
                 TableName: primaryTableName,
-                Key: {
-                    ItemId: `COMPANYUSER.${userSub}`,
-                    BelongsTo: `COMPANY.${companyId}`,
-                },
+                Key: primaryTableKeySearch,
             })
             .promise();
 
         const companyUser = getCompanyUserResult.Item as ICompanyUser;
         return companyUser.isCompanyAdmin;
     } catch (error) {
+        const awsError = error as AWS.AWSError;
         console.log("error checking if the user is a company admin");
+        console.log(awsError.message);
         return false;
     }
 }
