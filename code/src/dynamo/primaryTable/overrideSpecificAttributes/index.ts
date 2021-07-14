1caa16c9a26141fab7c1ade57797e5ef7b03080f
Chase <chasejarms@gmail.com>
2021-07-14 00:55:43 -0600

    Get the override function working

--- code/src/dynamo/primaryTable/overrideSpecificAttributes/index.ts
+++ code/src/dynamo/primaryTable/overrideSpecificAttributes/index.ts
@@ -25,7 +25,7 @@ export async function overrideSpecificAttributesInPrimaryTable<T>(
         Object.keys(itemAttributes).forEach((key, index) => {
             const value = itemAttributes[key];
             const valueAlias = `:${String.fromCharCode((index = 97))}`;
-            additionalUpdateExpressionValues.push(`info.${key}=${valueAlias}`);
+            additionalUpdateExpressionValues.push(`${key}=${valueAlias}`);
 
             expressionAttributeValuesInternal[valueAlias] = value;
         });
