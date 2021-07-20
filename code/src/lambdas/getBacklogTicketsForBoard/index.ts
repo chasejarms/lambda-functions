65e2f8d75043ee56f90d21721d34a936445abcfd
Chase <chasejarms@gmail.com>
2021-07-20 07:52:05 -0600

    Get the initial backlog ticket stuff done

---
+++ code/src/lambdas/getBacklogTicketsForBoard/index.ts
@@ -0,0 +1,58 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { queryStringParametersError } from "../../utils/queryStringParametersError";
+import { createErrorResponse } from "../../utils/createErrorResponse";
+import { HttpStatusCode } from "../../models/shared/httpStatusCode";
+import { isCompanyAdminOrBoardUser } from "../../utils/isCompanyAdminOrBoardUser";
+import { createAllInProgressTicketsKey } from "../../keyGeneration/createAllInProgressTicketsKey";
+import { queryParentToChildIndexBeginsWith } from "../../dynamo/parentToChildIndex/queryBeginsWith";
+import { ITicket } from "../../models/database/ticket";
+import { createSuccessResponse } from "../../utils/createSuccessResponse";
+import { createAllBacklogTicketsKey } from "../../keyGeneration/createAllBacklogTicketsKey";
+
+export const getBacklogTicketsForBoard = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    const queryStringParametersErrorMessage = queryStringParametersError(
+        event.queryStringParameters,
+        "boardId",
+        "companyId"
+    );
+    if (queryStringParametersErrorMessage) {
+        return createErrorResponse(
+            HttpStatusCode.BadRequest,
+            queryStringParametersErrorMessage
+        );
+    }
+
+    const { companyId, boardId } = event.queryStringParameters;
+
+    const canGetBacklogTicketsForBoard = await isCompanyAdminOrBoardUser(
+        event,
+        boardId,
+        companyId
+    );
+    if (!canGetBacklogTicketsForBoard) {
+        return createErrorResponse(
+            HttpStatusCode.Forbidden,
+            "insufficient permissions to get the backlog tickets for this board"
+        );
+    }
+
+    const allBacklogTicketsKey = createAllBacklogTicketsKey(companyId, boardId);
+
+    const tickets = await queryParentToChildIndexBeginsWith<ITicket>(
+        "C",
+        allBacklogTicketsKey
+    );
+
+    if (tickets === null) {
+        return createErrorResponse(
+            HttpStatusCode.BadRequest,
+            "Error getting the tickets for the board"
+        );
+    }
+
+    return createSuccessResponse({
+        tickets,
+    });
+};
