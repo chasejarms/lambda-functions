96d29cf2d63c96c95ee27375951f2af4bf5e899a
Chase <chasejarms@gmail.com>
2021-07-27 15:54:06 -0600

    Add get ticket template endpoint

---
+++ code/src/lambdas/getTicketTemplateForBoard/index.ts
@@ -0,0 +1,71 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { HttpStatusCode } from "../../models/shared/httpStatusCode";
+import { createErrorResponse } from "../../utils/createErrorResponse";
+import { createSuccessResponse } from "../../utils/createSuccessResponse";
+import { isCompanyAdminOrBoardUser } from "../../utils/isCompanyAdminOrBoardUser";
+import { createAllBoardTicketTemplatesKey } from "../../keyGeneration/createAllBoardTicketTemplatesKey";
+import { ITicketTemplate } from "../../models/database/ticketTemplate";
+import { queryStringParametersError } from "../../utils/queryStringParametersError";
+import { createBoardTicketTemplateKey } from "../../keyGeneration/createBoardTicketTemplateKey";
+import { getItemFromPrimaryTable } from "../../dynamo/primaryTable/getItem";
+
+export const getTicketTemplateForBoard = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    const queryStringParametersErrorMessage = queryStringParametersError(
+        event.queryStringParameters,
+        "boardId",
+        "companyId",
+        "ticketTemplateId"
+    );
+    if (queryStringParametersErrorMessage) {
+        return createErrorResponse(
+            HttpStatusCode.BadRequest,
+            queryStringParametersErrorMessage
+        );
+    }
+
+    const {
+        companyId,
+        boardId,
+        ticketTemplateId,
+    } = event.queryStringParameters;
+
+    const canGetTicketTemplateForBoard = await isCompanyAdminOrBoardUser(
+        event,
+        boardId,
+        companyId
+    );
+
+    if (!canGetTicketTemplateForBoard) {
+        return createErrorResponse(
+            HttpStatusCode.Forbidden,
+            "must be a company admin or a board user to get this ticket template"
+        );
+    }
+
+    const boardTicketTemplateKey = createBoardTicketTemplateKey(
+        companyId,
+        boardId,
+        ticketTemplateId
+    );
+
+    const allBoardTicketTemplatesKey = createAllBoardTicketTemplatesKey(
+        companyId,
+        boardId
+    );
+
+    const ticketTemplate = await getItemFromPrimaryTable<ITicketTemplate>(
+        boardTicketTemplateKey,
+        allBoardTicketTemplatesKey
+    );
+
+    if (ticketTemplate === null) {
+        return createErrorResponse(
+            HttpStatusCode.BadRequest,
+            "Error getting the ticket template from dynamo"
+        );
+    }
+
+    return createSuccessResponse(ticketTemplate);
+};
