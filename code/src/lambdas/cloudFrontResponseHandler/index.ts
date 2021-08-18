bbdc7533ca719fa02f709da25971f4f07a6fb318
Chase <chasejarms@gmail.com>
2021-08-18 17:53:36 -0600

    Get close to finishing deployment problem

--- code/src/lambdas/cloudFrontResponseHandler/index.ts
+++ code/src/lambdas/cloudFrontResponseHandler/index.ts
@@ -16,19 +16,21 @@ export const cloudFrontResponseHandler: CloudFrontResponseHandler = (
             value: "max-age=63072000; includeSubdomains; preload",
         },
     ];
+    // TODO: Investigate adding this back in later
     headers["content-security-policy"] = [
         {
             key: "Content-Security-Policy",
-            value: "default-src 'self';",
+            value: "default-src '*.com';",
         },
     ];
+    // headers["x-xss-protection"] = [
+    //     { key: "X-XSS-Protection", value: "1; mode=block" },
+    // ];
     headers["x-content-type-options"] = [
         { key: "X-Content-Type-Options", value: "nosniff" },
     ];
     headers["x-frame-options"] = [{ key: "X-Frame-Options", value: "DENY" }];
-    headers["x-xss-protection"] = [
-        { key: "X-XSS-Protection", value: "1; mode=block" },
-    ];
+
     headers["referrer-policy"] = [
         { key: "Referrer-Policy", value: "same-origin" },
     ];
